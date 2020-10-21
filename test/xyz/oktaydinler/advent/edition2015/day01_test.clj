0de01c7714a83ab195a3a35b8d8969d41cd12c5c
Oktay Dinler <oktay@oktaydinler.xyz>
2020-10-21 13:12:01 +0200

    Rename tests for 2015/1 to work with test toggle hotkey

--- test/xyz/oktaydinler/advent/edition2015/day01_test.clj
+++ test/xyz/oktaydinler/advent/edition2015/day01_test.clj
@@ -1,12 +1,12 @@
-(ns xyz.oktaydinler.advent.edition2015.day01_test
-  (:require [clojure.test :refer [deftest testing is]]
+(ns xyz.oktaydinler.advent.edition2015.day01-test
+  (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
-            [xyz.oktaydinler.advent.edition2015.day01 :refer [part1 part2]]))
+            [xyz.oktaydinler.advent.edition2015.day01 :refer :all]))
 
 (def puzzle-input
   (-> "input/2015/01.txt" io/resource slurp))
 
-(deftest part-1
+(deftest part1-test
   (testing "sample inputs"
     (is (= (part1 "(())") 0))
     (is (= (part1 "()()") 0))
@@ -20,7 +20,7 @@
   (testing "puzzle input"
     (is (= (part1 puzzle-input) 138))))
 
-(deftest part-2
+(deftest part2-test
   (testing "sample inputs"
     (is (= (part2, ")") 1))
     (is (= (part2, "()())") 5)))
