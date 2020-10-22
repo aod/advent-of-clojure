77b6e158e111a0a6eb228bdef6dd14e6b3289139
Oktay Dinler <oktay@oktaydinler.xyz>
2020-10-22 16:07:25 +0200

    Refactor 2015/2

--- src/xyz/oktaydinler/advent/edition2015/day02.clj
+++ src/xyz/oktaydinler/advent/edition2015/day02.clj
@@ -5,17 +5,15 @@
   [present]
   "Parses a dimension of a present which is length, width and height to a
   vector."
-  (into [] (map #(Integer/parseInt %) (str/split present #"x"))))
+  (into [] (map #(Integer/parseUnsignedInt %) (str/split present #"x"))))
 
 (defn wrapping-paper
   [present]
   "Calculates the amount of square feet of wrapping paper that is needed for the
   given present."
   (let [[l w h] present
-        lw (* l w)
-        wh (* w h)
-        hl (* h l)]
-    (+ (* 2 lw) (* 2 wh) (* 2 hl) (apply min [lw wh hl]))))
+        [lw wh hl] (map * [w h l] present)]
+    (+ (* 2 (+ lw wh hl)) (apply min [lw wh hl]))))
 
 (defn part1
   [presents]
@@ -29,13 +27,10 @@
 
 (defn ribbon
   [present]
-  "Calculates the amount of feet of ribbon to wrap the present and the ribbon
-  for the bow for the given present."
-  (let [[l w h] present
-        lw (* 2 (+ l w))
-        wh (* 2 (+ w h))
-        hl (* 2 (+ h l))]
-    (+ (* l w h) (apply min [lw wh hl]))))
+  "Calculates the amount of feet of ribbon to wrap the present and bow for the
+  given present."
+  (let [[l w h] present]
+    (+ (* l w h) (* 2 (- (+ l w h) (apply max [l w h]))))))
 
 (defn part2
   [presents]
