e52eb52be43c529d23d0725d28411834832ee71a
Oktay Dinler <oktay@oktaydinler.xyz>
2020-10-24 11:57:48 +0200

    Refactor 2015/5

--- src/xyz/oktaydinler/advent/edition2015/day05.clj
+++ src/xyz/oktaydinler/advent/edition2015/day05.clj
@@ -13,23 +13,17 @@
     (not-any? #(contains? bad-words %)
               (partition 2 1 string))))
 
-(defn part1 [input]
-  (let [is-nice (juxt has-vowels has-repeat no-bad-words)]
-    (count (filter true?
-                   (map #(every? true? (is-nice %))
-                        (str/split-lines input))))))
+(defn solve [is-nice-fn input]
+  (count (filter true?
+                 (map #(every? true? (is-nice-fn %))
+                      (str/split-lines input)))))
 
-(defn non-overlapped-repeat [string]
-  (let [p (Pattern/compile "(..).*\\1")
-        m (.matcher p string)]
-    (.find m)))
+(defn part1 [input] (solve (juxt has-vowels has-repeat no-bad-words) input))
+
+(defn non-overlapped-repeat [string] (some? (re-find #"(..).*\1" string)))
 
 (defn has-between [string]
   (some (fn [[l _ r]] (= l r))
         (partition 3 1 string)))
 
-(defn part2 [input]
-  (let [is-nice (juxt non-overlapped-repeat has-between)]
-    (count (filter true?
-                   (map #(every? true? (is-nice %))
-                        (str/split-lines input))))))
+(defn part2 [input] (solve (juxt non-overlapped-repeat has-between) input))
